4018fab5a246db530ae42d5ead9b72a8e37d30b4
Josué Chaves <d4n11083@estudiantec.cr>
2023-09-29 20:51:23 -0600

    Update MainActivity.kt

--- app/src/main/java/jochaes/minipythoncompiler/MainActivity.kt
+++ app/src/main/java/jochaes/minipythoncompiler/MainActivity.kt
@@ -82,12 +82,10 @@ class MainActivity : AppCompatActivity() {
                 println("Button Clicked")
         }
         })
-
 //        binding.fab.setOnClickListener { view ->
 //            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
 //                    .setAction("Action", null).show()
 //        }
-
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
